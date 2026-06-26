229182380a04560f224da197c8740a724136cf10
Ishtiak Billah Emon <ishtiakemon27@gmail.com>
2026-06-26 14:31:49 +0600

    login, logout, refresh-token routes endpoint created

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllercopy.js";
-import { loginUser } from "../controllers/user.controllercopy.js";
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controllercopy.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 router.post("/register", registerUser);
@@ -8,8 +8,11 @@ router.post("/login", loginUser);
 
 
 // secured route 
-router.post("/logout", verifyJWT, logoutUser)
+router.post("/logout",verifyJWT, logoutUser)
 
+
+// Token
+router.post("/refresh-token",refreshAccessToken)
 
 
 export default router;
