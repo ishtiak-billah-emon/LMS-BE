d15721b873c9306978cd4ea0a6284eef7ddd08bd
Ishtiak Billah Emon <ishtiakemon27@gmail.com>
2026-06-26 12:43:52 +0600

    Login and logout route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
-import { loginUser } from "../controllers/user.controller.js";
+import { registerUser } from "../controllers/user.controllercopy.js";
+import { loginUser } from "../controllers/user.controllercopy.js";
 
 const router = Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+
+// secured route 
+router.post("/logout", verifyJWT, logoutUser)
+
 
 
 export default router;
