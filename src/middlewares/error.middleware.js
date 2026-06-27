c496585ef7246e4b3115ffd7d5304f167bc35e52
Ishtiak Billah Emon <ishtiakemon27@gmail.com>
2026-06-27 16:30:39 +0600

    feat: Global error handler

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,16 @@
+const errorHandler = (err, req, res, next) => {
+
+    const statusCode = err.statusCode || 500;
+
+    return res.status(statusCode).json({
+        success: err.success ?? false,
+        message: err.message || "Internal Server Error",
+        errors: err.errors || [],
+        stack:
+            process.env.NODE_ENV === "development"
+                ? err.stack
+                : undefined,
+    });
+};
+
+export { errorHandler };
