1eb5fcb9e27bb6f2490898a1eaf69b49f7bdcc2c
Ishtiak Billah Emon <ishtiakemon27@gmail.com>
2026-07-14 13:02:44 +0600

    Get student routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,39 +1,42 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
-    registerUser,
-    loginUser,
-    logoutUser,
-    refreshAccessToken,
-    getCurrentUser,
-    changeCurrentPassword,
-    updateProfile,
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  getCurrentUser,
+  changeCurrentPassword,
+  updateProfile,
+  getMyCourses,
+  getStudents,
+  getStudentById,
 } from "../controllers/user.controllercopy.js";
-
 
 const router = Router();
 
 // ---------- Public Routes ----------
 
 router.post("/register", registerUser);
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 router.post("/refresh-token", refreshAccessToken);
 
-
 // ---------- Protected Routes ----------
-router.use((req, res, next) => {
-    console.log("ROUTER:", req.method, req.originalUrl);
-    next();
-  });
-  
+
 router.use(verifyJWT);
 
 router.post("/logout", logoutUser);
 
-router.get("/profile", getCurrentUser);
+router.get("/current-user", getCurrentUser);
 
 router.patch("/update-profile", updateProfile);
 
 router.patch("/change-password", changeCurrentPassword);
+
+router.get("/my-courses", getMyCourses);
+
+router.get("/total-students", getStudents);
+
+router.get("/:studentId", getStudentById);
 
 export default router;
